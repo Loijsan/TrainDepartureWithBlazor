d44ee529c97061ec6c2e3dbc4ab040e96955a27c
Louise Olsson <louise.m.k.olsson@hotmail.se>
2022-04-19 09:52:59 +0200

    Add project files.

---
+++ APIconnections/Models/JsonStationObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiConnections.Models
+{
+    public class JsonStationObject
+    {
+        public class Rootobject
+        {
+            public RESPONSE RESPONSE { get; set; }
+        }
+
+        public class RESPONSE
+        {
+            public RESULT[] RESULT { get; set; }
+        }
+
+        public class RESULT
+        {
+            public Trainstation[] TrainStation { get; set; }
+        }
+
+        public class Trainstation
+        {
+            public string AdvertisedLocationName { get; set; }
+            public string AdvertisedShortLocationName { get; set; }
+            public string LocationSignature { get; set; }
+        }
+    }
+}
---
+++ BlazorTrain/Models/TrainstationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorTrain.Models
+{
+    public class TrainstationModel
+    {
+        public string AdvertisedLocationName { get; set; }
+        public string AdvertisedShortLocationName { get; set; }
+        public string LocationSignature { get; set; }
+    }
+}
